f5ecfc370fc008fd8cf97d17b650723c1b43f42a
Thalita Neves <thalita.neves24@gmail.com>
2021-06-14 11:44:23 -0300

    Adição de botões de limpar tela, teste de função.

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,36 @@
 const cipher = {
-    const alphabet = 
-  };
+    encode: function criptografar(offset, string) {
+        let msgCodificada = "";
+        for(let i = 0; i < string.length; i++) {
+            let caractereCifrado = string.charCodeAt(i);
+        if(caractereCifrado >= 65 && caractereCifrado <= 90){
+           caractereCifrado = ((caractereCifrado - 65 + offset) % 26 + 65);
+        }
+        msgCodificada += String.fromCharCode(caractereCifrado);  
+        }
+    return msgCodificada;
+    },
+
+    decode: function descriptografar(offset, string){
+        let msgDesco = "";
+        for(let i = 0; i < string.length; i++) {
+            let caractereDescifrado = string.charCodeAt(i);
+        if(caractereDescifrado >= 65 && caractereDescifrado <= 90) {
+            caractereDescifrado = ((caractereDescifrado - 90 - offset) % 26 + 90);
+        }
+        msgDesco += String.fromCharCode(caractereDescifrado);
+        }
+    return msgDesco;
+    },
+    
+};
   
-  export default cipher; 
+export default cipher;
+
+
+
+
+
+
+
+
